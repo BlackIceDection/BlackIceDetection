e617e445fe3a229921684a756048d84690544eae
Matio Tawdrous <matiotawdrous@gmail.com>
2021-12-22 08:27:41 +0100

    python scripts finalised -one bug left

--- py scripts/BlackIceDetection.py
+++ py scripts/BlackIceDetection.py
@@ -6,7 +6,11 @@ import time
 from pathlib import Path
 import time,board,busio
 import numpy as np
+import adafruit_mlx90640
 import matplotlib.pyplot as plt
+from scipy import ndimage
+import base64
+import os
 
 DEVICE_BUS = 1
 DEVICE_ADDR = 0x17
@@ -23,13 +27,27 @@ BMP280_PRESSURE_REG_M = 0x0A
 BMP280_PRESSURE_REG_H = 0x0B
 BMP280_STATUS = 0x0C
 HUMAN_DETECT = 0x0D
+MQTT_IP = "192.168.0.101"
+
+i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # setup I2C
+mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ # set refresh rate
+mlx_shape = (24,32) # mlx90640 shape
+mlx_interp_val = 10 # interpolate # on each dimension
+mlx_interp_shape = (mlx_shape[0]*mlx_interp_val,
+                    mlx_shape[1]*mlx_interp_val) # new shape
+fig = plt.figure(figsize=(12,9)) # start figure
+ax = fig.add_subplot(111) # add subplot
+fig.subplots_adjust(0.05,0.05,0.95,0.95) # get rid of unnecessary padding
+therm1 = ax.imshow(np.zeros(mlx_interp_shape),interpolation='none',
+                cmap=plt.cm.bwr,vmin=25,vmax=45) # preemptive image
+cbar = fig.colorbar(therm1) # setup colorbar
 
 def readSensorData():
     sys.stdout = open('output.txt', 'w')
     bus = smbus.SMBus(DEVICE_BUS)
     aReceiveBuf = []
     aReceiveBuf.append(0x00)
-
     for i in range(TEMP_REG, HUMAN_DETECT + 1):
         aReceiveBuf.append(bus.read_byte_data(DEVICE_ADDR, i))
     if aReceiveBuf[STATUS_REG] & 0x01:
@@ -64,31 +82,45 @@ def readSensorData():
     else:
         print("No humans detected!")
     sys.stdout = sys.__stdout__
-
+	
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("$SYS/#")
-
-
+	
 # Publishes every 10 second the Data read from the sensors
 def on_message(client, userdata, msg):
-    #print(msg.topic+" "+str(msg.payload))
     readSensorData()
+    readThermalData()
     txt = open('output.txt').read()
-    print(txt)
-    publish.single("temperature", txt, hostname="192.168.43.4")
+    with open("thermalcam.png", "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    publish.single("temperature", txt, hostname=MQTT_IP)
+    publish.single("Schwarzi", encoded_string, hostname=MQTT_IP)
     time.sleep(10)
-
+	
+def readThermalData():
+    cbar.set_label('Temperature [$^{\circ}$C]',fontsize=14) # colorbar label
+    fig.canvas.draw() # draw figure to copy background
+    ax_background = fig.canvas.copy_from_bbox(ax.bbox) # copy background
+    fig.show() # show the figure before blitting
+    frame = np.zeros(mlx_shape[0]*mlx_shape[1]) # 768 pts
+    fig.canvas.restore_region(ax_background) # restore background
+    mlx.getFrame(frame) # read mlx90640
+    data_array = np.fliplr(np.reshape(frame,mlx_shape)) # reshape, flip data
+    data_array = ndimage.zoom(data_array,mlx_interp_val) # interpolate
+    therm1.set_array(data_array) # set data
+    therm1.set_clim(vmin=np.min(data_array),vmax=np.max(data_array)) # set bounds
+    cbar.update_normal(therm1) # update colorbar range
+    ax.draw_artist(therm1) # draw new thermal image
+    fig.canvas.blit(ax.bbox) # draw background
+    #fig.canvas.flush_events() # show the new image
+    fig.savefig('thermalcam.png', dpi=300, facecolor='#FCFCFC')
+	
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("192.168.43.4", 1883, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
+client.connect(MQTT_IP, 1883, 60)
 client.loop_forever()
